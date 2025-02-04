8cb9283e37e5622ce88cd9016ab06a45e59db9df
Lil-papa <candy1571@gmail.com>
2025-02-05 00:10:48 +0300

    change vite.config

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from "vitest/config"
 import react from "@vitejs/plugin-react"
 
 export default defineConfig({
-  base: "react-redux-game",
+  base: "/react-redux-game/",
   plugins: [react()],
   server: {
     open: true,
